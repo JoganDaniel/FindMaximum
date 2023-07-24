96f156107d60e4ca3bc81e01c282c60ee9133214
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-24 15:34:55 +0530

    [jogan] Add : Folder structure created

---
+++ FindMaximum/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace FindMaximum
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+            operation.FindMaxInteger(1,2,3);
+            
+        }
+    }
+}
